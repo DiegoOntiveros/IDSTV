ca43a46fd292f5aff13c0554fda571ff53b048a5
DiegoOntiveros <dontiveros_23@alu.uabcs.mx>
2025-02-19 20:06:21 -0800

    Initial commit

---
+++ src/IDSTV/Main.java
@@ -0,0 +1,25 @@
+package IDSTV;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("odio eclipse");
+		
+		
+		window objeto = new window();
+	 
+	 
+		JFrame window = new JFrame();
+		
+
+	}
+}
---
+++ src/IDSTV/window.java
@@ -0,0 +1,32 @@
+package IDSTV;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.security.PrivateKey;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class window extends JFrame {
+
+	public window() {
+		
+		
+this.setVisible(true);
+		
+		this.setTitle("textos");
+		this.setSize(500, 500);
+		
+	 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	 this.setResizable(true);
+	 this.setMaximumSize(new Dimension (800,400));
+	 this.setMinimumSize(new Dimension(400,400));
+		 this.setLocationRelativeTo(null);
+		 
+
+
+	}
+}
