6b4d8349c703e331df9bae3c70142118938a0be9
DiegoOntiveros <dontiveros_23@alu.uabcs.mx>
2025-02-25 11:46:36 -0800

    Commit unidad 2 ejercicio 17 o diseño del login
    se agrego imagenes al login y se uso el jpasswordtexfield para la contraseña y se agrego un color no predeterminado se cambio la tipografia de Arial a Arial black que es la que usa el ine.

--- src/IDSTV/window.java
+++ src/IDSTV/window.java
@@ -1,48 +1,53 @@
 package IDSTV;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class window extends JFrame {
-
+public static  final Color COLOR_BASE = new Color(221,201, 163);
 	public window() {
-	
+
 		
 this.setVisible(true);
 		
-		this.setTitle("textos");
+		this.setTitle("Regristo datos para el ine");
 		this.setSize(500, 500);
 		
 	 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	 this.setResizable(true);
-	 this.setMaximumSize(new Dimension (800,500));
-	 this.setMinimumSize(new Dimension(500,600));
+	 this.setMaximumSize(new Dimension (800,800));
+	 this.setMinimumSize(new Dimension(650,800));
 		 this.setLocationRelativeTo(null);
 	        this.setLayout(null);
-	        
+	     this.getContentPane().setBackground(COLOR_BASE);
+	       
 //============================================================================================	 
 		
 		 JLabel textobase = new JLabel();
 		 textobase.setText("Regristo de datos");
 		 textobase.setHorizontalAlignment(SwingConstants.CENTER);
 		 textobase.setVerticalAlignment(SwingConstants.CENTER);
-		 textobase.setBounds(85, 15, 320, 50);
-		 textobase.setFont (new Font("Arial", Font.BOLD,24));
+		 textobase.setBounds(20, 15, 320, 50);
+		 textobase.setFont (new Font("Arial black", Font.BOLD,24));
 		 textobase.setBackground(Color.white);
 		 textobase.setOpaque(true);
 		 textobase.setForeground(Color.black);
@@ -56,24 +61,46 @@ this.setVisible(true);
 		 Nombre.setText("Ingrese su nombre completo:");
 		 Nombre.setHorizontalAlignment(SwingConstants.CENTER);
 		 Nombre.setVerticalAlignment(SwingConstants.CENTER);
-		 Nombre.setBounds(20, 80, 210,50);
-		 Nombre.setFont (new Font("Arial", Font.BOLD,14));
-		 //Nombre.setBackground(Color.white);
+		 Nombre.setBounds(10, 80, 350,50);
+		 Nombre.setFont (new Font("Arial black", Font.BOLD,20));
+		 Nombre.setBackground(COLOR_BASE);
 		 Nombre.setOpaque(true);
 		 Nombre.setForeground(Color.black);
 		 this.add(Nombre);
 		 		 
 		 this.setVisible(true);
 		 
+		 JLabel Password = new JLabel();
+		 Password.setText("Ingrese su contraseña:");
+		 Password.setHorizontalAlignment(SwingConstants.CENTER);
+		 Password.setVerticalAlignment(SwingConstants.CENTER);
+		 Password.setBounds(10, 180, 280,50);
+		 Password.setFont (new Font("Arial black", Font.BOLD,20));
+		 Password.setBackground(COLOR_BASE);
+		 Password.setOpaque(true);
+		 Password.setForeground(Color.black);
+		 this.add(Password);
+		 		 
+		 this.setVisible(true);
+		 
 		 
 //============================================================================================	 
-
+		 //BARRAS DE PONER TEXTO Y PONER CONTRASENA:
+		 
 		 JTextField barra_nombre = new JTextField();
 		 barra_nombre.setBounds(20, 130, 250, 30);
 	
 		 this.add(barra_nombre);
 		 this.setVisible(true);
 		 
+		 // BARRA PARA CONTRASENA, Y QUE NOS PERMITE NO VER LA CONTRASENA EN LA PANTALLA
+		 JPasswordField barra_password = new JPasswordField();
+		 barra_password.setBounds(20, 240, 250, 30);
+	
+		 this.add(barra_password);
+		 this.setVisible(true);
+		 
+		
 		 
 //============================================================================================	 
 		 
@@ -81,48 +108,63 @@ this.setVisible(true);
 		 Ciudad.setText("Escoja su ciudad:");
 		 Ciudad.setHorizontalAlignment(SwingConstants.CENTER);
 		 Ciudad.setVerticalAlignment(SwingConstants.CENTER);
-		 Ciudad.setBounds(20, 170, 140,50);
-		 Ciudad.setFont (new Font("Arial", Font.BOLD,15));
-		 //Nombre.setBackground(Color.white);
+		 Ciudad.setBounds(15, 300, 220,50);
+		 Ciudad.setFont (new Font("Arial black", Font.BOLD,20));
+		 Ciudad.setBackground(COLOR_BASE);
 		 Ciudad.setOpaque(true);
 		 Ciudad.setForeground(Color.black);
 		 this.add(Ciudad);
 		 this.setVisible(false);
 		 
+		 
+		 //opcion para poder escojer una ciudad:
+		 
 		 String[] ciudades = {"Cabo san lucas","La paz","San jose","Ciudad constitucion"};
 
 		 JComboBox lista = new JComboBox(ciudades);
-		 lista.setBounds(20,220, 150,50);
+		 lista.setBounds(20,360, 150,80);
 		 this.add(lista);
 		 
 //============================================================================================	 	 
+		 //opciones de genero por boton 
 		 
 		 JLabel Genero = new JLabel();
 		 Genero.setText("Escoja su Genero:");
 		 Genero.setHorizontalAlignment(SwingConstants.CENTER);
 		 Genero.setVerticalAlignment(SwingConstants.CENTER);
-		 Genero.setBounds(20, 170, 140,280);
-		 Genero.setFont (new Font("Arial", Font.BOLD,15));
-		 //Nombre.setBackground(Color.white);
+		 Genero.setBounds(10, 350, 250,280);
+		 Genero.setFont (new Font("Arial black", Font.BOLD,20));
+		 Genero.setBackground(COLOR_BASE);
 		 Genero.setOpaque(true);
 		 Genero.setForeground(Color.black);
 		 this.add(Genero);
 		 this.setVisible(false);
 
-		 
+		 //opcion 1
 		 JRadioButton opcion_genero = new JRadioButton("Hombre",false);
-		 opcion_genero.setBounds(20, 330, 170,40);
+		 opcion_genero.setBounds(20, 510, 170,40);
 		 this.add(opcion_genero);
+		 opcion_genero.setBackground(COLOR_BASE);
 		 this.setVisible(false);
-		 
+		 opcion_genero.setFont (new Font("Arial", Font.ITALIC,15));
+
+		 // opcion 2
 		 JRadioButton opcion_genero2 = new JRadioButton("Mujer",false);
-		 opcion_genero2.setBounds(20, 360, 170,40);
+		 opcion_genero2.setBounds(20, 550, 170,40);
 		 this.add(opcion_genero2);
-		 this.setVisible(false);
-		 
+		 opcion_genero2.setBackground(COLOR_BASE);
+		 this.setVisible(true);
+		 opcion_genero2.setFont (new Font("Arial", Font.ITALIC,15));
+
+		// opcion 3
 		 JRadioButton opcion_genero3 = new JRadioButton("No definido",false);
-		 opcion_genero3.setBounds(20, 390, 170,40);
+		 opcion_genero3.setBounds(20, 590, 170,40);
 		 this.add(opcion_genero3);
+		 opcion_genero3.setBackground(COLOR_BASE);
+		 opcion_genero3.setFont (new Font("Arial", Font.ITALIC,15));
+
+		 
+
 		 this.setVisible(false);
 
 		 ButtonGroup opciones = new ButtonGroup();
@@ -131,8 +173,10 @@ this.setVisible(true);
 		 opciones.add(opcion_genero3);
 		 
 //============================================================================================	 
+		 //boton de regristo:
+		 
 		 JButton boton = new JButton();
-		 boton.setBounds(165, 500, 150, 50);
+		 boton.setBounds(50, 680, 150, 50);
 		 boton.setText("Regristarce");
 		 boton.setForeground(Color.BLACK);
 		 boton.setFont (new Font("Arial", Font.BOLD,15));
@@ -143,6 +187,18 @@ this.setVisible(true);
 		 boton.setOpaque(true);
 		 this.setVisible(true);
 		 
+		 //icono del ine tamano:
+		 JLabel logo = new JLabel();
+	        logo.setBounds(300, 250, 250, 250); 
+	        this.add(logo);
+	        
+	     //poner la imagen el tamano y la calidad de esta:
+	        
+	     ImageIcon imagenIcon = new ImageIcon("src/logo.png"); 
+	     Image imagen = imagenIcon.getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+	     logo.setIcon(new ImageIcon(imagen));
+
+	      this.setVisible(true);
 		 
 		 
 	}
